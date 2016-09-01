420da4ccc12076d7838bc10ed1a42a83df0196d1
Muhammad Fahad <mfahadfadii@gmail.com>
2016-09-01 14:17:41 +0500

    initial setup

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by Fahad on 9/1/2016.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+platformBrowserDynamic().bootstrapModule(AppModule);
